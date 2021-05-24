a30e24f74c26c7f5ff3d6d29afd7d69df699e6ce
xq0107 <3425126218@qq.com>
2021-05-24 15:13:29 +0800

    默认变更列表

--- work/app.py
+++ work/app.py
@@ -8,12 +8,20 @@ app = Flask(__name__)
 filename = r"doc/release.xlsx"
 sheet_BCA = "BCA"
 sheet_plan = "plan"
+sheet_links="links"
+
 
 @app.route('/table')
 def talbe():
-    plan_info=read_doc(filename=filename,sheetname=sheet_plan,max_line=8)
-    return render_template("table.html", plan_info=plan_info)
+    # 发布计划
+    plan_info = read_doc(filename=filename, sheetname=sheet_plan, max_line=8)
+
+    # BC
+    BC_info =read_doc(filename=filename, sheetname=sheet_BCA)
 
 
+#links
+    links =read_doc(filename=filename, sheetname=sheet_BCA)
+    return render_template("table.html", plan_info=plan_info,BC_info=BC_info,links=links)
 if __name__ == "__main__":
     app.run(debug=True)
