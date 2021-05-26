b26c44f5799ba1ba55307b334fdcc10957148e9d
xq0107 <3425126218@qq.com>
2021-05-26 16:58:25 +0800

    work_0526

--- F_work/app.py
+++ F_work/app.py
@@ -1,9 +1,25 @@
 # -*- coding:utf-8 -*-
 
 from flask import *
+from read import *
+
+filename = r"doc/release.xlsx"
+sheet_BCA = "BCA"
+sheet_plan = "plan"
+sheet_links = "links"
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    # 发布计划
+    plan_info = read_doc(filename=filename, sheetname=sheet_plan, max_line=8)
+
+    # links
+    links = read_doc(filename=filename, sheetname=sheet_links)
+    links_dct = make_dct(links)
+    return render_template("home.html", plan_info=plan_info, links=links_dct)
+
+
+app.run(debug=True)
