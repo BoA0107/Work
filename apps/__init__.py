1e93ff2bc802d926ebda25960d42606dff021227
BoA0107 <2446912060@qq.com>
2021-06-24 14:27:18 +0800

    aaaaa

--- apps/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import *
-from apps.show.view import show_bp
-def create_app():
-    app=Flask(__name__)
-    app.register_blueprint(show_bp)
--- apps/home/view.py
+++
@@ -1,7 +0,0 @@
-from flask import *
-
-home_bp = Blueprint('home', __name__)
-
-@home_bp.route('/home')
-def home():
-    return 'home'
